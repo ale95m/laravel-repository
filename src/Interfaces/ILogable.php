a53031bc21ed292117860393bc555eedf417080a
Alejandro Moreno <44046329+ale95m@users.noreply.github.com>
2021-05-26 16:05:14 -0400

    Update ILogable.php

--- src/Interfaces/ILogable.php
+++ src/Interfaces/ILogable.php
@@ -9,8 +9,6 @@ use Illuminate\Support\Collection;
  */
 interface ILogable
 {
-    function getLogModel(): string;
-
     function getLogData(bool $with_relations, bool $include_null, ?array $only_attributes = null): Collection;
 
     function getLogableAttributes(): array;

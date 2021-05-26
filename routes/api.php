97450359f5807eaad8ffb9748c0e1d59ddc71e11
Alejandro Moreno <44046329+ale95m@users.noreply.github.com>
2021-05-25 22:52:23 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,12 @@ use Illuminate\Http\Request;
 
 if (config('easy.use_auth')) {
     Route::prefix('auth')->group(function () {
-        Route::post('login', [AuthController::class, 'login']);
+        Route::post('login', [AuthController::class, 'login'])->name('auth.login');
         Route::middleware(['auth:api'])->group(function () {
             Route::get('user', function (Request $request) {
                 return $request->user();
             })->name('auth.user');
-            Route::get('logout', [AuthController::class, 'logout']);
+            Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
         });
     });
 }

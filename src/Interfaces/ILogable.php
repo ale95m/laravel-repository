ec7612a13fd6c133e66e2c7225f53bcc7be63d87
Alejandro Moreno <44046329+ale95m@users.noreply.github.com>
2021-05-27 19:38:45 -0400

    Revert "Update ILogable.php"
    This reverts commit a53031bc21ed292117860393bc555eedf417080a.

--- src/Interfaces/ILogable.php
+++ src/Interfaces/ILogable.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Collection;
  */
 interface ILogable
 {
+    function getLogModel(): string;
+
     function getLogData(bool $with_relations, bool $include_null, ?array $only_attributes = null): Collection;
 
     function getLogableAttributes(): array;

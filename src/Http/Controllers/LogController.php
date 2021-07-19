721403c3366e5d6186dbf8fd4411e3652d73050c
Alejandro Moreno <44046329+ale95m@users.noreply.github.com>
2021-07-19 17:56:22 -0400

    remove paginate everride in logControler

--- src/Http/Controllers/LogController.php
+++ src/Http/Controllers/LogController.php
@@ -2,8 +2,6 @@
 
 namespace Easy\Http\Controllers;
 
-use Easy\Http\Requests\PaginateRequest;
-use Easy\Http\Responses\SendResponse;
 use Easy\Repositories\LogRepository;
 
 class LogController extends EasyController
@@ -11,19 +9,5 @@ class LogController extends EasyController
     public function __construct(LogRepository $repository)
     {
         $this->repository = $repository;
-    }
-
-    public function paginate(PaginateRequest $request)
-    {
-        $per_page = $request['itemsPerPage'] ?? 20;
-        $current_page = $request['page'] ?? 1;
-        try {
-            $paginator = $this->repository->search($request->all())
-                ->paginate($per_page, ['*'], 'page', $current_page);
-            $logs = $this->repository->map($paginator->items());
-            return SendResponse::successLogsPagination($paginator, $logs);
-        } catch (\Exception $e) {
-            return SendResponse::error($e->getMessage());
-        }
     }
 }

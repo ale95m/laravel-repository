25a96c421ebfe7f5fa4517319ec6e36e973c8dbe
Alejandro Moreno <44046329+ale95m@users.noreply.github.com>
2022-11-19 10:54:10 -0500

    custom auth middleware

--- routes/api.php
+++ routes/api.php
@@ -8,22 +8,20 @@ use Illuminate\Http\Request;
 
 if (config('easy.use_auth')) {
     Route::prefix('auth')->group(function () {
-        Route::post('password/reset', [AuthController::class,'reset']);
-        Route::post('password/forgot', [AuthController::class,'forgot']);
-        Route::get('password/restore', [AuthController::class,'restorePassword'])->name('password.reset');
+        Route::post('password/reset', [AuthController::class, 'reset']);
+        Route::post('password/forgot', [AuthController::class, 'forgot']);
+        Route::get('password/restore', [AuthController::class, 'restorePassword'])->name('password.reset');
         Route::post('login', [AuthController::class, 'login'])->name('auth.login');
-        Route::middleware(['auth:api'])->group(function () {
+        Route::middleware(config('easy.auth_middleware'))->group(function () {
             Route::get('user', function (Request $request) {
                 $user = $request->user()->load(config('easy.auth_user_relations'));
-                return config('easy.json_numeric_check')
-                    ? json_encode($user, JSON_NUMERIC_CHECK)
-                    : json_encode($user);
+                return json_encode($user);
             })->name('auth.user');
             Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
         });
     });
     if (config('easy.email_verification')) {
-        Route::prefix('emailVerify')->middleware(['auth:api'])->group(function () {
+        Route::prefix('emailVerify')->middleware(config('easy.auth_middleware'))->group(function () {
             Route::get('{user}', [VerificationController::class, 'verify'])->name('verification.verify');
             Route::get('', [VerificationController::class, 'resend'])->name('verification.resend');
         });
